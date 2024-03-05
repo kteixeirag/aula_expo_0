5053347a315fecfb20479536fc38c967b7681700
kteixeirag <160647502+kteixeirag@users.noreply.github.com>
2024-03-05 15:24:50 -0300

    aula 05-03

---
+++ components/exemplo_0/index.js
@@ -0,0 +1,13 @@
+import {View, Text } from 'react-native';
+
+import styles from './styles';
+
+function exemplo_0 () {
+    return(
+        <View style={styles.container}>
+            <Text>Minha primeira tela</Text>
+            </View>
+    );
+}
+
+export default exemplo_0;
