6eb1980d853863e606431116e100440d77a1d875
kteixeirag <160647502+kteixeirag@users.noreply.github.com>
2024-03-12 15:23:42 -0300

    12/03

---
+++ components/atividade_1/index.js
@@ -0,0 +1,14 @@
+import {View, Text } from 'react-native';
+
+import styles from './styles';
+
+function Atividade1 () {
+    return(
+        <View style={styles.container}>
+            <Text>Atividade1</Text>
+            <Text>Beicin</Text>
+            </View>
+    );
+}
+
+export default Atividade1;
