e2c06e5004f2b42aab142a33fb18c2d406a75375
kteixeirag <160647502+kteixeirag@users.noreply.github.com>
2024-03-19 15:23:26 -0300

    19/03

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Exemplo_2 from './components/exemplo_2';
 import Exemplo_0 from './components/exemplo_1';
 
 import Atividade1 from './components/atividade_1';
@@ -8,7 +8,7 @@ import Atividade1 from './components/atividade_1';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Atividade1 />
+      <Exemplo_2 />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/exemplo_2/index.js
@@ -0,0 +1,13 @@
+import {View, Text } from 'react-native';
+
+import styles from './styles';
+
+function Exemplo_2 () {
+    return(
+        <View style={styles.container}>
+            <Text>Exemplo_2</Text>
+            </View>
+    );
+}
+
+export default Exemplo_2;
